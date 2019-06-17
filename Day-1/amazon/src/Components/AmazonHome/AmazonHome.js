0fa2117925a92ce80357727ba9382136141f48e4
Ravikiran303 <ravikiran2221@gmail.com>
2019-06-17 19:15:32 +0530

    Add amazon home component

---
+++ Day-1/amazon/src/Components/AmazonHome/AmazonHome.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Products from "../Products/Products";
+
+export class AmazonHome extends Component {
+  render() {
+    return (
+      <div>
+        <Products />
+      </div>
+    );
+  }
+}
+
+export default AmazonHome;
