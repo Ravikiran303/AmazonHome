141cc1af00fa1c64d478d5bcc804ca8c4472afcf
Ravikiran303 <ravikiran2221@gmail.com>
2019-06-17 19:14:23 +0530

    Create amazon home page project

---
+++ Day-1/amazon/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import AmazonHome from "./Components/AmazonHome/AmazonHome";
+
+function App() {
+  return (
+    <div className="App">
+      <AmazonHome />
+    </div>
+  );
+}
+
+export default App;
